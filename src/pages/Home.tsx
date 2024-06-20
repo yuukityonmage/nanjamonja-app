c0c059cddf93964b7ff61b1fed768e61327018f5
yuukityonmage <yuukisoccerkandai@gmail.com>
2024-06-20 13:39:29 +0900

    ホームのロゴ画像修正

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <img src="./images/logo.jpg" className="App-logo" alt="logo" />
+                <img src="../images/logo.jpg" className="App-logo" alt="logo" />
                 <p>
                 オリジナルモンジャ
                 </p>
